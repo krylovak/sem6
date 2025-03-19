bde76e55b4371e401f6e4b973bde2196af7ae8fc
krylovak <166594179+krylovak@users.noreply.github.com>
2025-03-19 12:15:24 +0300

    Крестики Нолики

---
+++ tic_tac_toe_rus/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TicTacToe from "./TicTacToe";
+
+function App() {
+  return (
+    <div>
+      <h1>Крестики-нолики</h1>
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
