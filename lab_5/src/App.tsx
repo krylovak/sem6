7b7353f6579c4f7ac5eef02d91e94e35ce8b4d7b
krylovak <166594179+krylovak@users.noreply.github.com>
2025-04-16 12:41:12 +0300

    лаба 5

--- lab_5/src/App.tsx
+++ lab_5/src/App.tsx
@@ -1,14 +1,19 @@
-import { Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./App.css";
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import HomePage from './pages/Home';
+import EditPage from './pages/Edit';
 
 function App() {
-  return {
+  return (
     <div className="App">
       <h1>Meme Generator</h1>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/edit" element={<EditPage />} />
+      </Routes>
     </div>
-  };
+  );
 }
 
 export default App;
--- lab_5/src/components/Card.tsx
+++ lab_5/src/components/Card.tsx
@@ -1,17 +1,20 @@
-import React from "react";
-import Button from "react-bootstrap/Button"; 
-import Card from "react-bootstrap/Card";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { useNavigate } from 'react-router-dom';
 
-const MemeCard = () = {
+const MemeCard = (props: any) => {
+    const navigate = useNavigate();
     return (
-        <Card style={{ width: "18rem" }}>
-        <Card. Img variant="top" src="holder-js/100px180" />
-            <Card. Body>
-            ‹Card.Title>Card Title</Card.Title>
-            ‹Card. Text>
-            Some quick example text to build on the card title and make bulk of the card's contenty-</Card. Text>
-            ‹Button variant="primary">Go somewhere</Button>
-            </Card. Body>
+        <Card style={{ width: '18rem', margin: "25px" }}>
+        <Card.Img variant="top" src={props.img} />
+        <Card.Body>
+            <Card.Title>{props.title}</Card.Title>
+            <Button onClick={(e) => navigate(`/edit?url=${props.img}`)} variant="primary">
+                Edit
+            </Button>
+        </Card.Body>
         </Card>
-    ｝；
-    export default MemeCard;
+    );
+}
+
+export default MemeCard;
